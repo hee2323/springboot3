8ebbd37df1ac90babb531763f228fa124a318a27
재희 <hareppy329@gmail.com>
2024-08-26 03:04:48 +0900

    [셀프체크]회원정보 수정하기

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO article(id, title, content) VALUES(1, '가가가', '111');
 INSERT INTO article(id, title, content) VALUES(2, '나나나', '222');
 INSERT INTO article(id, title, content) VALUES(3, '다다다', '333');
+
+INSERT INTO member(id, email, password) VALUES(1, '123@gmail.net', '11111');
+INSERT INTO member(id, email, password) VALUES(2, '234@gmail.net', '22222');
+INSERT INTO member(id, email, password) VALUES(3, '345@gmail.net', '33333');
